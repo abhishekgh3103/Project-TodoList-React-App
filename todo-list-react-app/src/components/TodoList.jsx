31fbe99de17ac1ad0f7abf194ac6d9c851f02781
Abhishek Govindaraj <abhishekgh3103@gmail.com>
2024-01-26 15:17:32 +0530

    Add sorting to todoList

--- todo-list-react-app/src/components/TodoList.jsx
+++ todo-list-react-app/src/components/TodoList.jsx
@@ -2,9 +2,10 @@ import TodoItem from "./TodoItem";
 import styles from "./todoList.module.css"
 
 export default function TodoList({todos, setTodos}) {
+    const sortedTodos = todos.slice().sort((a,b) => Number(a.done) - Number(b.done))
     return(
         <div className={styles.list}>
-        {todos.map((item) => (
+        {sortedTodos.map((item) => (
         <TodoItem item={item} key={item.name} todos={todos} setTodos={setTodos}/>
         ))}
         </div>
