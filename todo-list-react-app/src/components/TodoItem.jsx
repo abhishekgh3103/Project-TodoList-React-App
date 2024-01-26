18f2eec744fdc51b22d7171117e8e479a73748e7
Abhishek Govindaraj <abhishekgh3103@gmail.com>
2024-01-26 13:35:17 +0530

    Add delete functionality to todoitem component

--- todo-list-react-app/src/components/TodoItem.jsx
+++ todo-list-react-app/src/components/TodoItem.jsx
@@ -1,8 +1,15 @@
 import styles from "./todoItem.module.css"
-export default function TodoItem({item}) {
+export default function TodoItem({item, todos, setTodos}) {
+    function handleClick(item) {
+        console.log('click delete', item);
+        setTodos(todos.filter(todo => todo !== item));
+    }
     return (
     <div className={styles.item}>
-        <div className={styles.itemName}>{item}</div>
+        <div className={styles.itemContainer}>
+            <div className={styles.itemName}>{item}</div>
+            <button className={styles.itemButton} onClick={() =>handleClick(item)}>X</button>
+        </div>
         <hr className= {styles.line}/>
     </div>
     );
--- todo-list-react-app/src/components/TodoList.jsx
+++ todo-list-react-app/src/components/TodoList.jsx
@@ -1,11 +1,11 @@
 import TodoItem from "./TodoItem";
 import styles from "./todoList.module.css"
 
-export default function TodoList({todos}) {
+export default function TodoList({todos, setTodos}) {
     return(
         <div className={styles.list}>
         {todos.map((item) => (
-        <TodoItem item={item} key={item}/>
+        <TodoItem item={item} key={item} todos={todos} setTodos={setTodos}/>
         ))}
         </div>
         );
